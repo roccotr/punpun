1fc785cda02873ed484d05ed09366a915c58c1be
sommoyogurt <rtrombetta@gmail.com>
2015-08-31 14:33:28 +0200

    Add some function :)

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ extern crate fern;
 extern crate time;
 
 extern crate mio;
-extern crate env_logger;
+extern crate rmp;
+extern crate rmp_serialize;
+extern crate rustc_serialize;
 
 use log::*;
 
@@ -19,26 +21,32 @@ use mio::buf::ByteBuf;
 use mio::tcp::*;
 use mio::util::Slab;
 
+use rustc_serialize::{Encodable,Decodable};
+use rmp_serialize::{Encoder,Decoder};
+
 
 const HOST: &'static str = "0.0.0.0:10001";
+
+#[derive(RustcEncodable, RustcDecodable, PartialEq, Debug)]
+struct Message {
+    ch: String,
+    msg: String,
+    time: String
+}
 
 
 
 
 fn start_server() {
-    // Before doing anything, let us register a logger. The mio library has really good logging
-    // at the _trace_ and _debug_ levels. Having a logger setup is invaluable when trying to
-    // figure out why something is not working correctly.
+
     let logger_config = fern::DispatchConfig {
         format: Box::new(|msg: &str, level: &log::LogLevel, _location: &log::LogLocation| {
-            // This is a fairly simple format, though it's possible to do more complicated ones.
-            // This closure can contain any code, as long as it produces a String message.
             format!("[{}] - {} - {}", time::now().strftime("%Y-%m-%d:%H:%M:%S").unwrap(), level, msg)
          }),
         output: vec![fern::OutputConfig::stdout(), fern::OutputConfig::file("output.log")],
-        level: log::LogLevelFilter::Trace,
+        level: log::LogLevelFilter::Debug,
     };
-    if let Err(e) = fern::init_global_logger(logger_config, log::LogLevelFilter::Trace) {
+    if let Err(e) = fern::init_global_logger(logger_config, log::LogLevelFilter::Debug) {
         panic!("Failed to initialize global logger: {}", e);
     }
     //env_logger::init().ok().expect("Failed to init logger");
@@ -49,10 +57,6 @@ fn start_server() {
 
     let mut event_loop = EventLoop::new().ok().expect("Failed to create event loop");
 
-    // Create our Server object and register that with the event loop. I am hiding away
-    // the details of how registering works inside of the `Server#register` function. One reason I
-    // really like this is to get around having to have `const SERVER = Token(0)` at the top of my
-    // file. It also keeps our polling options inside `Server`.
     let mut server = Server::new(sock);
     server.register(&mut event_loop).ok().expect("Failed to register server with event loop");
 
@@ -67,15 +71,9 @@ fn main() {
 
 
 struct Server {
-    // main socket for our server
     sock: TcpListener,
-
-    // token of our server. we keep track of it here instead of doing `const SERVER = Token(0)`.
     token: Token,
-
-    // a list of connections _accepted_ by our server
     conns: Slab<Connection>,
-
 }
 
 impl Handler for Server {
@@ -98,8 +96,6 @@ impl Handler for Server {
             return;
         }
 
-        // We never expect a write event for our `Server` token . A write event for any other token
-        // should be handed off to that connection.
         if events.is_writable() {
             trace!("Write event for {:?}", token);
             assert!(self.token != token, "Received writable event for Server");
@@ -111,9 +107,6 @@ impl Handler for Server {
                     self.reset_connection(event_loop, token);
                 });
         }
-
-        // A read event for our `Server` token means we are establishing a new connection. A read
-        // event for any other token should be handed off to that connection.
         if events.is_readable() {
             trace!("Read event for {:?}", token);
             if self.token == token {
@@ -135,22 +128,11 @@ impl Server {
     fn new(sock: TcpListener) -> Server {
         Server {
             sock: sock,
-
-            // I don't use Token(0) because kqueue will send stuff to Token(0)
-            // by default causing really strange behavior. This way, if I see
-            // something as Token(0), I know there are kqueue shenanigans
-            // going on.
             token: Token(1),
-
-            // SERVER is Token(1), so start after that
-            // we can deal with a max of 126 connections
             conns: Slab::new_starting_at(Token(2), 128)
         }
     }
 
-    /// Register Server with the event loop.
-    ///
-    /// This keeps the registration details neatly tucked away inside of our implementation.
     fn register(&mut self, event_loop: &mut EventLoop<Server>) -> io::Result<()> {
         event_loop.register_opt(
             &self.sock,
@@ -163,9 +145,6 @@ impl Server {
         })
     }
 
-    /// Register Server with the event loop.
-    ///
-    /// This keeps the registration details neatly tucked away inside of our implementation.
     fn reregister(&mut self, event_loop: &mut EventLoop<Server>) {
         event_loop.reregister(
             &self.sock,
@@ -179,15 +158,9 @@ impl Server {
         })
     }
 
-    /// Accept a _new_ client connection.
-    ///
-    /// The server will keep track of the new connection and forward any events from the event loop
-    /// to this connection.
     fn accept(&mut self, event_loop: &mut EventLoop<Server>) {
         debug!("server accepting new socket");
 
-        // Log an error if there is no socket, but otherwise move on so we do not tear down the
-        // entire server.
         let sock = match self.sock.accept() {
             Ok(s) => {
                 match s {
@@ -206,20 +179,11 @@ impl Server {
             }
         };
 
-        // `Slab#insert_with` is a wrapper around `Slab#insert`. I like `#insert_with` because I
-        // make the `Token` required for creating a new connection.
-        //
-        // `Slab#insert` returns the index where the connection was inserted. Remember that in mio,
-        // the Slab is actually defined as `pub type Slab<T> = ::slab::Slab<T, ::Token>;`. Token is
-        // just a tuple struct around `usize` and Token implemented `::slab::Index` trait. So,
-        // every insert into the connection slab will return a new token needed to register with
-        // the event loop. Fancy...
         match self.conns.insert_with(|token| {
             debug!("registering {:?} with event loop", token);
             Connection::new(sock, token)
         }) {
             Some(token) => {
-                // If we successfully insert, then register our connection.
                 match self.find_connection_by_token(token).register(event_loop) {
                     Ok(_) => {},
                     Err(e) => {
@@ -229,20 +193,12 @@ impl Server {
                 }
             },
             None => {
-                // If we fail to insert, `conn` will go out of scope and be dropped.
                 error!("Failed to insert connection into slab");
             }
         };
-
-        // We are using edge-triggered polling. Even our SERVER token needs to reregister.
         self.reregister(event_loop);
     }
 
-    /// Forward a readable event to an established connection.
-    ///
-    /// Connections are identified by the token provided to us from the event loop. Once a read has
-    /// finished, push the receive buffer into the all the existing connections so we can
-    /// broadcast.
     fn readable(&mut self, event_loop: &mut EventLoop<Server>, token: Token) -> io::Result<()> {
         debug!("server conn readable; token={:?}", token);
         let message = try!(self.find_connection_by_token(token).readable());
@@ -258,14 +214,16 @@ impl Server {
         for conn in self.conns.iter_mut() {
             // TODO: use references so we don't have to clone
             let conn_send_buf = ByteBuf::from_slice(message.bytes());
-            conn.send_message(conn_send_buf)
-                .and_then(|_| conn.reregister(event_loop))
-                .unwrap_or_else(|e| {
-                    error!("Failed to queue message for {:?}: {:?}", conn.token, e);
-                    // We have a mutable borrow for the connection, so we cannot remove until the
-                    // loop is finished
-                    bad_tokens.push(conn.token)
-                });
+            if token != conn.token {
+                conn.send_message(conn_send_buf)
+                    .and_then(|_| conn.reregister(event_loop))
+                    .unwrap_or_else(|e| {
+                        error!("Failed to queue message for {:?}: {:?}", conn.token, e);
+                        // We have a mutable borrow for the connection, so we cannot remove until the
+                        // loop is finished
+                        bad_tokens.push(conn.token)
+                    });
+            }
         }
 
         for t in bad_tokens {
@@ -290,20 +248,10 @@ impl Server {
     }
 }
 
-/// A stateful wrapper around a non-blocking stream. This connection is not
-/// the SERVER connection. This connection represents the client connections
-/// _accepted_ by the SERVER connection.
 struct Connection {
-    // handle to the accepted socket
     sock: TcpStream,
-
-    // token used to register with the event loop
     token: Token,
-
-    // set of events we are interested in
     interest: EventSet,
-
-    // messages waiting to be sent out
     send_queue: Vec<ByteBuf>,
 }
 
@@ -312,56 +260,21 @@ impl Connection {
         Connection {
             sock: sock,
             token: token,
-
-            // new connections are only listening for a hang up event when
-            // they are first created. we always want to make sure we are
-            // listening for the hang up event. we will additionally listen
-            // for readable and writable events later on.
             interest: EventSet::hup(),
-
             send_queue: Vec::new(),
         }
     }
 
-    /// Handle read event from event loop.
-    ///
-    /// Currently only reads a max of 2048 bytes. Excess bytes are dropped on the floor.
-    ///
-    /// The recieve buffer is sent back to `Server` so the message can be broadcast to all
-    /// listening connections.
     fn readable(&mut self) -> io::Result<ByteBuf> {
-
-        // ByteBuf is a heap allocated slice that mio supports internally. We use this as it does
-        // the work of tracking how much of our slice has been used. I chose a capacity of 2048
-        // after reading
-        // https://github.com/carllerche/mio/blob/eed4855c627892b88f7ca68d3283cbc708a1c2b3/src/io.rs#L23-27
-        // as that seems like a good size of streaming. If you are wondering what the difference
-        // between messaged based and continuous streaming read
-        // http://stackoverflow.com/questions/3017633/difference-between-message-oriented-protocols-and-stream-oriented-protocols
-        // . TLDR: UDP vs TCP. We are using TCP.
         let mut recv_buf = ByteBuf::mut_with_capacity(2048);
-
-        // we are PollOpt::edge() and PollOpt::oneshot(), so we _must_ drain
-        // the entire socket receive buffer, otherwise the server will hang.
         loop {
             match self.sock.try_read_buf(&mut recv_buf) {
-                // the socket receive buffer is empty, so let's move on
-                // try_read_buf internally handles WouldBlock here too
                 Ok(None) => {
                     debug!("CONN : we read 0 bytes");
                     break;
                 },
                 Ok(Some(n)) => {
                     debug!("CONN : we read {} bytes", n);
-
-                    // if we read less than capacity, then we know the
-                    // socket is empty and we should stop reading. if we
-                    // read to full capacity, we need to keep reading so we
-                    // can drain the socket. if the client sent exactly capacity,
-                    // we will match the arm above. the recieve buffer will be
-                    // full, so extra bytes are being dropped on the floor. to
-                    // properly handle this, i would need to push the data into
-                    // a growable Vec<u8>.
                     if n < recv_buf.capacity() {
                         break;
                     }
@@ -372,17 +285,9 @@ impl Connection {
                 }
             }
         }
-
-        // change our type from MutByteBuf to ByteBuf
         Ok(recv_buf.flip())
     }
 
-    /// Handle a writable event from the event loop.
-    ///
-    /// Send one message from the send queue to the client. If the queue is empty, remove interest
-    /// in write events.
-    /// TODO: Figure out if sending more than one message is optimal. Maybe we should be trying to
-    /// flush until the kernel sends back EAGAIN?
     fn writable(&mut self) -> io::Result<()> {
 
         try!(self.send_queue.pop()
@@ -391,8 +296,6 @@ impl Connection {
                 match self.sock.try_write_buf(&mut buf) {
                     Ok(None) => {
                         debug!("client flushing buf; WouldBlock");
-
-                        // put message back into the queue so we can try again
                         self.send_queue.push(buf);
                         Ok(())
                     },
@@ -415,20 +318,11 @@ impl Connection {
         Ok(())
     }
 
-    /// Queue an outgoing message to the client.
-    ///
-    /// This will cause the connection to register interests in write events with the event loop.
-    /// The connection can still safely have an interest in read events. The read and write buffers
-    /// operate independently of each other.
     fn send_message(&mut self, message: ByteBuf) -> io::Result<()> {
         self.send_queue.push(message);
         self.interest.insert(EventSet::writable());
         Ok(())
     }
-
-    /// Register interest in read events with the event_loop.
-    ///
-    /// This will let our connection accept reads starting next event loop tick.
     fn register(&mut self, event_loop: &mut EventLoop<Server>) -> io::Result<()> {
         self.interest.insert(EventSet::readable());
 
@@ -443,7 +337,6 @@ impl Connection {
         })
     }
 
-    /// Re-register interest in read events with the event_loop.
     fn reregister(&mut self, event_loop: &mut EventLoop<Server>) -> io::Result<()> {
         event_loop.reregister(
             &self.sock,
@@ -459,7 +352,9 @@ impl Connection {
 
 #[cfg(test)]
 mod Test {
-    use super::{start_server,HOST};
+    use rustc_serialize::{Encodable,Decodable};
+    use rmp_serialize::{Encoder,Decoder};
+    use super::{start_server,HOST, Message};
     use std::io::prelude::*;
     use std::net::TcpStream;
     use std::thread;
@@ -473,7 +368,9 @@ mod Test {
     #[test]
     fn client(){
         thread::sleep_ms(3000);
-
+        let smsg = Message{ch:"po".to_string(),msg:"po".to_string(),time:"po".to_string()};
+        let mut buf = "pipo".as_bytes();
+        //smsg.encode(&mut Encoder::new(&mut &mut buf[..]));
         for i in 0..10 {
 
             let _ = thread::spawn(move|| {
@@ -481,7 +378,7 @@ mod Test {
                 let mut stream = TcpStream::connect(HOST).unwrap();
 
                 for x in 0..100 {
-                    write!(stream, "the answer is {}", i).unwrap();
+                    stream.write(&buf);
 
                     let mut r = [0u8; 256];
                     match stream.read(&mut r) {
